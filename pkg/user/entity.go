7d48e5ec0fa0c420b4ed6a8ba591b2a524c6cddc
Rithik Jain <rithik.jain3006@gmail.com>
2020-04-22 01:26:46 +0530

    Modified user entity

--- pkg/user/entity.go
+++ pkg/user/entity.go
@@ -2,13 +2,16 @@ package user
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/rithikjain/SocialRecipe/pkg/recipe"
 )
 
 type User struct {
 	gorm.Model
-	Name        string `json:"name"`
-	PhoneNumber string `json:"phone_number"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	Bio         string `json:"bio"`
+	Name          string          `json:"name"`
+	PhoneNumber   string          `json:"phone_number"`
+	Email         string          `json:"email"`
+	Password      string          `json:"password"`
+	ProfileImgUrl string          `json:"profile_img_url"`
+	Bio           string          `json:"bio"`
+	Recipes       []recipe.Recipe `json:"-" gorm:"foreignkey:UserID"`
 }
