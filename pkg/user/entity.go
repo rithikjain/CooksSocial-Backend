a3a408d3de9501152bfec3f1a36dfaec408c56a0
Rithik Jain <rithik.jain3006@gmail.com>
2020-04-21 17:57:52 +0530

    Added user entity

---
+++ pkg/user/entity.go
@@ -0,0 +1,12 @@
+package user
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Name        string `json:"name"`
+	PhoneNumber string `json:"phone_number"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	Bio         string `json:"bio"`
+}
